1c40f08c331852060b12d830da8036d8f57cec14
Enopoletus <33143044+Enopoletus@users.noreply.github.com>
2017-11-22 19:15:32 -0500

    Add files via upload

---
+++ Results replacement.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name         Results replacement
+// @namespace    https://enopoletus.github.io
+// @version      0.1
+// @description  Replaces link to framed version with no-frame version of page on uselectionatlas.org
+// @author       You
+// @include      *
+// @grant        none
+// @run-at document-start
+// ==/UserScript==
+
+window.onload=function overwrites(){var x = document.getElementsByTagName("a");
+for (i=0;i<x.length;++i) {
+  if (x[i].getAttribute("href") == "RESULTS/") {
+x[i].setAttribute("href", "https://uselectionatlas.org/RESULTS/national.php?year=2016&f=0&off=0&elect=0");}}
+var q = document.getElementsByTagName("a");
+for (i=0;i<q.length;++i) {
+  if (q[i].getAttribute("href") == "https://uselectionatlas.org/RESULTS/") {
+q[i].setAttribute("href", "https://uselectionatlas.org/RESULTS/national.php?year=2016&f=0&off=0&elect=0");}}
+var u = document.getElementsByTagName("a");
+for (i=0;i<u.length;++i) {
+  if (u[i].getAttribute("href") == "http://uselectionatlas.org/RESULTS/") {
+u[i].setAttribute("href", "https://uselectionatlas.org/RESULTS/national.php?year=2016&f=0&off=0&elect=0");}}
+                                   };
