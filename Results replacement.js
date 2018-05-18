74124d2cf34d48cedfbb718e3dc6fb06fafcf136
Enopoletus Harding <33143044+Enopoletus@users.noreply.github.com>
2018-05-17 22:30:31 -0400

    Update Results replacement.js

--- Results replacement.js
+++ Results replacement.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Results replacement
 // @namespace    https://enopoletus.github.io
-// @version      0.1
+// @version      0.11
 // @description  Replaces link to framed version with no-frame version of page on uselectionatlas.org
 // @author       You
 // @include      *
@@ -10,15 +10,15 @@
 // ==/UserScript==
 
 window.addEventListener("load", function overwrites(){var x = document.getElementsByTagName("a");
-for (i=0;i<x.length;++i) {
+for (let i of x) {
   if (x[i].getAttribute("href") == "RESULTS/") {
 x[i].setAttribute("href", "https://uselectionatlas.org/RESULTS/national.php?year=2016&f=0&off=0&elect=0");}}
 var q = document.getElementsByTagName("a");
-for (i=0;i<q.length;++i) {
+for (let i of q) {
   if (q[i].getAttribute("href") == "https://uselectionatlas.org/RESULTS/") {
 q[i].setAttribute("href", "https://uselectionatlas.org/RESULTS/national.php?year=2016&f=0&off=0&elect=0");}}
 var u = document.getElementsByTagName("a");
-for (i=0;i<u.length;++i) {
+for (let i of u) {
   if (u[i].getAttribute("href") == "http://uselectionatlas.org/RESULTS/") {
 u[i].setAttribute("href", "https://uselectionatlas.org/RESULTS/national.php?year=2016&f=0&off=0&elect=0");}}
                                    });
