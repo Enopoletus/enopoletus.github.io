d0c907fd9c55db8c1cca1ae21acd4bb8d2732d3b
Enopoletus Harding <33143044+Enopoletus@users.noreply.github.com>
2019-07-31 11:43:39 -0400

    Update mpdreconcladd.py

--- mpdreconcladd.py
+++ mpdreconcladd.py
@@ -51,7 +51,7 @@ for a in yournames:
         ratir=ratir[maskr]
         # 1953 to include African and exclude most socialist countries
         bmk=dfi[q].loc[1500:1953].last_valid_index()
-        if bmk==None or q in ('TWN', 'KOR):
+        if bmk==None or q in ('TWN', 'KOR'):
             ratiq=ratir
         else:
             weight1=abs(ratic.index-2011)/(2011-bmk)
